eb2144ac79dbd7a8568ff2a146a1966b76f820f0
Travis Ford <116747699+Wcminer-Trav@users.noreply.github.com>
2023-11-09 11:45:41 -0600

    Changes 11/9

--- DataProcessor.py
+++ DataProcessor.py
@@ -6,72 +6,111 @@ import time
 import talib
 import logging
 import threading
+import numpy as np
 from pybit.unified_trading import HTTP
 from queue import Queue
 
 # Setting up logging
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+expected_columns = {
+    'timestamp': 'datetime64[ns]',
+    'symbol': 'object',
+    'side': 'float64',
+    'volume': 'float64',
+    'price': 'float64',
+    'tick_direction': 'object',
+    'trade_id': 'object',
+    'is_buy_trade': 'object'
+}
+
+expected_columns = {
+    'timestamp': 'datetime64[ns]',
+    'symbol': 'object',
+    'side': 'int64',
+    'volume': 'float64',
+    'price': 'float64',
+    'tick_direction': 'int64',
+    'trade_id': 'int64',
+    'is_buy_trade': 'bool',
+}
+
 
 class DataProcessor:
-    def __init__(self, data_queue):
-        self.data_df = pd.DataFrame()
-        self.processed_data_list = []  # List to store processed data before converting to DataFrame
+    def __init__(self, data_queue, window_size=50):
         self.data_queue = data_queue
+        self.window_size = window_size
+        self.data_df = pd.DataFrame(columns=expected_columns.keys()).astype(expected_columns)
+        self.processed_data_list = []
+        self.technical_indicators_calculated = False
+        logging.getLogger().setLevel(logging.INFO)
 
-    def process_message(self, processed_data):
-        # Convert the single trade data to a DataFrame
-        trade_df = pd.DataFrame([processed_data])
+    def process_message(self, message):
+        try:
+            if not isinstance(message, (pd.DataFrame, pd.Series)):
+                logging.error(f"Expected message to be a DataFrame or Series, but got {type(message)}")
+                if isinstance(message, dict):
+                    message = pd.DataFrame([message])
+                    for col in expected_columns:
+                        if col not in message.columns:
+                            message[col] = np.nan
 
-        # Concatenate the new data to the existing DataFrame
-        self.data_df = pd.concat([self.data_df, trade_df], ignore_index=True)
+            # At this point, 'message' must be a DataFrame, append to list
+            self.processed_data_list.append(message)
 
-        # Calculate technical indicators if enough data is available
-        self.calculate_technical_indicators()
+            # When enough data is collected
+            if len(self.processed_data_list) >= self.window_size:
+                self.data_df = pd.concat(self.processed_data_list).astype(expected_columns)
+                self.processed_data_list = []
+                self.calculate_technical_indicators()
 
-        # Process RSI
-        if 'RSI' in self.data_df.columns:
-            last_rsi = self.data_df['RSI'].iloc[-1]
-            if last_rsi > 70:
-                logging.info(f"Overbought RSI: {last_rsi}")
-            elif last_rsi < 30:
-                logging.info(f"Oversold RSI: {last_rsi}")
+                # Handling non-finite values and ensuring correct data types
+                self.data_df['side'] = pd.to_numeric(self.data_df['side'], errors='coerce').fillna(0).astype(int)
+                self.data_df['volume'] = pd.to_numeric(self.data_df['volume'], errors='coerce')
+                self.data_df['tick_direction'] = pd.to_numeric(self.data_df['tick_direction'], errors='coerce').fillna(
+                    0).astype(int)
+                self.data_df['trade_id'] = pd.to_numeric(self.data_df['trade_id'], errors='coerce').fillna(0).astype(
+                    int)
+                self.data_df['is_buy_trade'] = self.data_df['is_buy_trade'].astype(bool)
 
-        # Process MACD
-        if 'MACD' in self.data_df.columns and 'MACDSIGNAL' in self.data_df.columns:
-            last_macd = self.data_df['MACD'].iloc[-1]
-            last_macd_signal = self.data_df['MACDSIGNAL'].iloc[-1]
-            if last_macd > last_macd_signal:
-                logging.info(f"MACD crossover: {last_macd} (MACD) > {last_macd_signal} (Signal)")
-            elif last_macd < last_macd_signal:
-                logging.info(f"MACD crossunder: {last_macd} (MACD) < {last_macd_signal} (Signal)")
+            self.data_queue.put(self.data_df.tail(1))
+            logging.info("Data enqueued. Queue size: %d", self.data_queue.qsize())
+            logging.debug("Enqueued data: %s", message)
 
-        # Process Bollinger Bands
-        if 'Bollinger Upper' in self.data_df.columns and 'Bollinger Lower' in self.data_df.columns:
-            last_close = self.data_df['price'].iloc[-1]
-            last_upper_band = self.data_df['Bollinger Upper'].iloc[-1]
-            last_lower_band = self.data_df['Bollinger Lower'].iloc[-1]
-            if last_close > last_upper_band:
-                logging.info(f"Price above upper Bollinger Band: {last_close} > {last_upper_band}")
-            elif last_close < last_lower_band:
-                logging.info(f"Price below lower Bollinger Band: {last_close} < {last_lower_band}")
+            if self.technical_indicators_calculated:
+                if self.data_df['RSI'].iloc[-1] > 70:
+                    logging.info(f"Overbought RSI: {self.data_df['RSI'].iloc[-1]}")
+                elif self.data_df['RSI'].iloc[-1] < 30:
+                    logging.info(f"Oversold RSI: {self.data_df['RSI'].iloc[-1]}")
+                if self.data_df['MACD'].iloc[-1] > self.data_df['MACDSIGNAL'].iloc[-1]:
+                    logging.info(
+                        f"MACD crossover: {self.data_df['MACD'].iloc[-1]} (MACD) > {self.data_df['MACDSIGNAL'].iloc[-1]} (Signal)")
+                elif self.data_df['MACD'].iloc[-1] < self.data_df['MACDSIGNAL'].iloc[-1]:
+                    logging.info(
+                        f"MACD crossunder: {self.data_df['MACD'].iloc[-1]} (MACD) < {self.data_df['MACDSIGNAL'].iloc[-1]} (Signal)")
+                if self.data_df['price'].iloc[-1] > self.data_df['Bollinger Upper'].iloc[-1]:
+                    logging.info(
+                        f"Price above upper Bollinger Band: {self.data_df['price'].iloc[-1]} > {self.data_df['Bollinger Upper'].iloc[-1]}")
+                elif self.data_df['price'].iloc[-1] < self.data_df['Bollinger Lower'].iloc[-1]:
+                    logging.info(
+                        f"Price below lower Bollinger Band: {self.data_df['price'].iloc[-1]} < {self.data_df['Bollinger Lower'].iloc[-1]}")
+                if self.data_df['Stochastic K'].iloc[-1] > 80:
+                    logging.info(f"Stochastic Overbought: %K = {self.data_df['Stochastic K'].iloc[-1]}")
+                elif self.data_df['Stochastic K'].iloc[-1] < 20:
+                    logging.info(f"Stochastic Oversold: %K = {self.data_df['Stochastic K'].iloc[-1]}")
+                if self.data_df['Stochastic K'].iloc[-1] > self.data_df['Stochastic D'].iloc[-1]:
+                    logging.info(
+                        f"Stochastic Crossover: %K = {self.data_df['Stochastic K'].iloc[-1]} > %D = {self.data_df['Stochastic D'].iloc[-1]}")
+                elif self.data_df['Stochastic K'].iloc[-1] < self.data_df['Stochastic D'].iloc[-1]:
+                    logging.info(
+                        f"Stochastic Crossunder: %K = {self.data_df['Stochastic K'].iloc[-1]} < %D = {self.data_df['Stochastic D'].iloc[-1]}")
 
-        # Process Stochastic
-        if 'Stochastic K' in self.data_df.columns and 'Stochastic D' in self.data_df.columns:
-            last_stoch_k = self.data_df['Stochastic K'].iloc[-1]
-            last_stoch_d = self.data_df['Stochastic D'].iloc[-1]
-            if last_stoch_k > 80:
-                logging.info(f"Stochastic Overbought: %K = {last_stoch_k}")
-            elif last_stoch_k < 20:
-                logging.info(f"Stochastic Oversold: %K = {last_stoch_k}")
-            if last_stoch_k > last_stoch_d:
-                logging.info(f"Stochastic Crossover: %K = {last_stoch_k} > %D = {last_stoch_d}")
-            elif last_stoch_k < last_stoch_d:
-                logging.info(f"Stochastic Crossunder: %K = {last_stoch_k} < %D = {last_stoch_d}")
 
-        # Optionally, you can also trim the DataFrame to keep only recent data in memory
-        if len(self.data_df) > 2000:
-            self.data_df = self.data_df.iloc[-2000:]
+        except AttributeError as e:
+            logging.error(f"AttributeError in process_message: {e}")
+        except Exception as e:
+            logging.error(f"Unexpected error in process_message: {e}")
+        
 
     def fetch_bybit_data(self):
         try:
@@ -79,29 +118,35 @@ class DataProcessor:
             data = session.get_kline(category="inverse", symbol="BTCUSDT", interval="1", limit=2000)
             logging.info("Fetched historical data")
 
-            df = pd.DataFrame(data)
-            self.data_df = pd.concat([self.data_df, df], ignore_index=True)
+            self.processed_data_list.append(pd.DataFrame(data))
             self.calculate_technical_indicators()
         except Exception as e:
             logging.error(f"Error fetching Bybit data: {e}")
 
     def calculate_technical_indicators(self):
         try:
-            window_size = 50
-            if len(self.data_df) >= window_size:
-                close = self.data_df['price'].astype(float).values
-                high = self.data_df['price'].astype(float).values
-                low = self.data_df['price'].astype(float).values
+            window_size = 50  # Define window_size here
+            if len(self.data_df) >= self.window_size:  # Use self.window_size instead of window_size
+                close = self.data_df['price'].astype(float).values[-self.window_size:]  # Use self.window_size
+                high = self.data_df['price'].astype(float).values[-self.window_size:]  # Use self.window_size
+                low = self.data_df['price'].astype(float).values[-self.window_size:]  # Use self.window_size
 
-                self.data_df['RSI'] = talib.RSI(close, timeperiod=14)
-                self.data_df['MACD'], self.data_df['MACDSIGNAL'], _ = talib.MACD(close, fastperiod=12, slowperiod=26,
-                                                                                 signalperiod=9)
-                self.data_df['Bollinger Upper'], self.data_df['Bollinger Middle'], self.data_df[
-                    'Bollinger Lower'] = talib.BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+                rsi = talib.RSI(close, timeperiod=14)
+                macd, macd_signal, _ = talib.MACD(close, fastperiod=12, slowperiod=26, signalperiod=9)
+                bb_upper, bb_middle, bb_lower = talib.BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
                 slowk, slowd = talib.STOCH(high, low, close, fastk_period=5, slowk_period=3, slowk_matype=0,
                                            slowd_period=3, slowd_matype=0)
-                self.data_df['Stochastic K'] = slowk
-                self.data_df['Stochastic D'] = slowd
+
+                self.data_df['RSI'].iloc[-window_size:] = rsi
+                self.data_df['MACD'].iloc[-window_size:] = macd
+                self.data_df['MACDSIGNAL'].iloc[-window_size:] = macd_signal
+                self.data_df['Bollinger Upper'].iloc[-window_size:] = bb_upper
+                self.data_df['Bollinger Middle'].iloc[-window_size:] = bb_middle
+                self.data_df['Bollinger Lower'].iloc[-window_size:] = bb_lower
+                self.data_df['Stochastic K'].iloc[-window_size:] = slowk
+                self.data_df['Stochastic D'].iloc[-window_size:] = slowd
+
+                self.technical_indicators_calculated = True
         except Exception as e:
             logging.error(f"Error calculating technical indicators: {e}")
 
@@ -113,13 +158,49 @@ class DataProcessor:
         except Exception as e:
             logging.error(f"Error opening WebSocket: {e}")
 
+    def check_dataframe_format(self):
+        if not isinstance(self.data_df, pd.DataFrame):
+            logging.error(f"self.data_df is not a DataFrame: {type(self.data_df)}")
+            return
+
+        missing_values = self.data_df[expected_columns.keys()].isnull().sum()
+        for col, missing_count in missing_values.items():
+            if missing_count > 0:
+                logging.warning(f"Missing values detected in column {col}: {missing_count} rows")
+
+        for col, expected_type in expected_columns.items():
+            if col in self.data_df.columns:
+                col_type = self.data_df[col].dtype
+                if not isinstance(col_type, np.dtype):  # Check if col_type is not a numpy dtype object
+                    logging.error(f"The dtype of column {col} is not valid: {type(self.data_df[col])}")
+                    continue  # Skip this iteration
+                if str(col_type) != expected_type:
+                    logging.warning(
+                        f"Unexpected data type for column {col}. Expected {expected_type} but got {col_type}")
+
     def on_message(self, ws, message):
         try:
             data = json.loads(message)
             logging.info("Received a message")
 
+            self.process_data_message(data)
+
+            self.check_dataframe_format()
+
+        except json.JSONDecodeError as e:
+            logging.error(f"Error decoding JSON: {e}")
+        except KeyError as e:
+            logging.error(f"Missing expected key in data: {e}")
+        except Exception as e:
+            logging.error(f"Error processing WebSocket message: {e}")
+
+    def process_data_message(self, data):
+        try:
             if 'topic' in data and data['topic'] == 'publicTrade.BTCUSDT':
                 for trade_data in data['data']:
+                    # Log the type of trade_data to ensure it's what we expect (should be a dict)
+                    logging.debug(f"Type of trade_data: {type(trade_data)}")
+
                     timestamp = trade_data['T']
                     processed_data = {
                         'timestamp': pd.to_datetime(timestamp, unit='ms'),
@@ -131,16 +212,13 @@ class DataProcessor:
                         'trade_id': trade_data['i'],
                         'is_buy_trade': trade_data['BT']
                     }
-                    self.processed_data_list.append(processed_data)
-
-                self.process_message(processed_data)  # Call the process_message method of the current instance
-
-        except json.JSONDecodeError as e:
-            logging.error(f"Error decoding JSON: {e}")
-        except KeyError as e:
-            logging.error(f"Missing expected key in data: {e}")
+                    # Log the type of processed_data to ensure it's a dict
+                    logging.debug(f"Type of processed_data: {type(processed_data)}")
+                    self.process_message(processed_data)
+        except AttributeError as e:
+            logging.error(f"AttributeError in process_data_message: {e}")
         except Exception as e:
-            logging.error(f"Error processing WebSocket message: {e}")
+            logging.error(f"Unexpected error in process_data_message: {e}")
 
     def on_error(self, ws, error):
         logging.error(f"WebSocket error: {error}")
@@ -159,11 +237,13 @@ class DataProcessor:
             logging.error(f"Error saving data: {e}")
 
     def run(self):
-        self.fetch_bybit_data()
+        if len(self.data_df) < 1000:
+            self.fetch_bybit_data()
         schedule.every(30).minutes.do(self.fetch_bybit_data)
 
         endpoint = "wss://stream.bybit.com/v5/public/linear"
-        ws = websocket.WebSocketApp(endpoint, on_open=self.on_open, on_message=self.on_message, on_error=self.on_error, on_close=self.on_close)
+        ws = websocket.WebSocketApp(endpoint, on_open=self.on_open, on_message=self.on_message, on_error=self.on_error,
+                                    on_close=self.on_close)
 
         ws_thread = threading.Thread(target=ws.run_forever)
         ws_thread.start()
--- LandonBot.py
+++ LandonBot.py
@@ -1,13 +1,28 @@
-import pandas as pd
-import numpy as np
+from tensorflow.keras.layers import Dense, Reshape, LSTM
+from tensorflow.keras.utils import get_custom_objects
+from tensorflow.keras import mixed_precision
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras import layers
+from tensorflow.keras.metrics import Mean
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+from tensorflow.keras.optimizers import AdamW
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+import pandas as pd
+import numpy as np
 import queue
 import logging
 import time
-from tensorflow.keras import mixed_precision
 from sklearn.preprocessing import MinMaxScaler
+import traceback
+
+from tensorflow.keras.mixed_precision import set_global_policy
+
+
+
+set_global_policy('mixed_float16')
 
 
 class LandonBot:
@@ -23,43 +38,76 @@ class LandonBot:
             "Stochastic Oscillator"
         ]
         logging.info("LandonBot initialized")
+        self.check_gpu_support()
 
-    def build_model(self):
-        # Enable mixed precision training
-        policy = mixed_precision.Policy('mixed_float16')
-        mixed_precision.set_global_policy(policy)
+    def check_gpu_support(self):
+        if tf.config.experimental.list_physical_devices('GPU'):
+            policy = mixed_precision.Policy('mixed_float16')
+            mixed_precision.set_global_policy(policy)
+
+    def build_model(self, num_neurons=64, initial_learning_rate=0.001, decay_steps=10000, decay_rate=0.9,
+                    num_features=16, seq_length=60):
+        """
+        Builds a model for regression using the given parameters.
+
+        Args:
+            num_neurons (int): Number of neurons for the Dense and LSTM layers.
+            initial_learning_rate (float): Initial learning rate for the optimizer.
+            decay_steps (int): Number of steps for the learning rate decay.
+            decay_rate (float): Rate of decay for the learning rate.
+            num_features (int): Number of features for the input shape.
+            seq_length (int): Length of the sequence for the input shape.
+
+        Returns:
+            tf.keras.Model: The built model.
+        """
 
         # Build the model
         model = keras.Sequential([
-            layers.Dense(64, activation='relu', input_shape=(60, 14)),
-            layers.LSTM(64, return_sequences=True),  # Set to False if you only need the last output
-            layers.LSTM(64),  # This layer will only return the last output
-            layers.Dense(1, activation='sigmoid', dtype='float32')
+            Dense(num_neurons, activation='relu', input_shape=(seq_length, num_features)),
+            # Specify the input shape here
+            Reshape((seq_length, num_neurons)),
+            LSTM(num_neurons, return_sequences=True),
+            LSTM(num_neurons),
+            Dense(1, activation='linear', dtype='float32')
         ])
-        model.compile(optimizer='adam', loss='mean_squared_error')
+
+        # Use AdamW optimizer with learning rate scheduler
+        learning_rate_scheduler = ExponentialDecay(
+            initial_learning_rate=initial_learning_rate,
+            decay_steps=decay_steps,
+            decay_rate=decay_rate
+        )
+        optimizer = AdamW(learning_rate=learning_rate_scheduler, weight_decay=0.01)
+
+        # Use mean squared error as the loss function
+        model.compile(optimizer=optimizer, loss=MeanSquaredError())
+
         return model
 
     def preprocess_data(self, df):
         # Handle missing values
         df = df.ffill().bfill()
 
-        # Drop non-numeric columns and convert to float32 for memory efficiency
-        df = df.drop(columns=["timestamp", "symbol"]).astype('float32')
+        # Convert non-numeric columns to appropriate data types
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        df['symbol'] = df['symbol'].astype('category')
 
         # Normalization
-        df = self.scaler.fit_transform(df)
+        df[['volume', 'price', 'tick_direction', 'trade_id', 'is_buy_trade', 'High', 'Low', 'close', 'RSI', 'MACD', 'Bollinger Upper', 'Bollinger Lower', 'Stochastic Oscillator']] = self.scaler.transform(df[['volume', 'price', 'tick_direction', 'trade_id', 'is_buy_trade', 'High', 'Low', 'close', 'RSI', 'MACD', 'Bollinger Upper', 'Bollinger Lower', 'Stochastic Oscillator']])
+        logging.info(f"Processing data: {df}")
         return df
 
     def create_sequences(self, data, seq_length):
         sequences = []
-        for i in range(len(data) - seq_length):
+        for i in range(0, len(data) - seq_length, seq_length):
             seq = data[i:i + seq_length]
             label = data[i + seq_length, -1]
             sequences.append((seq, np.array([label])))
         return np.array(sequences, dtype=object)
 
-    def train(self, data, batch_size=512):  # Add batch_size parameter
-        if len(data) < 61:  # Ensure there is enough data
+    def train_model(self, data, batch_size):
+        if len(data) < 61:
             logging.warning("Not enough data to create sequences for training. Data length: %s", len(data))
             return
         sequences = self.create_sequences(data, 60)
@@ -67,7 +115,7 @@ class LandonBot:
         self.model.fit(X, y, epochs=10, batch_size=batch_size, verbose=0)
 
     def predict(self, data):
-        if len(data) < 61:  # Ensure there is enough data
+        if len(data) < 61:
             logging.warning("Not enough data to create sequences for prediction. Data length: %s", len(data))
             return
         sequences = self.create_sequences(data, 60)
@@ -75,48 +123,99 @@ class LandonBot:
         predictions = self.model.predict(X)
         logging.info("Predictions: %s", predictions)
 
-    def run(self, batch_size=512):  # Add batch_size parameter
+    def run(self, batch_size=512):
         logging.info("Starting LandonBot")
         tf.profiler.experimental.start("E:\\Documents")
         while True:
-            try:
-                queue_size = self.data_queue.qsize()
-                logging.info("Data queue size: %d", queue_size)
+            queue_size = self.data_queue.qsize()
+            logging.info("Data queue size: %d", queue_size)
 
-                if not self.data_queue.empty():
-                    new_data = pd.DataFrame(self.data_queue.get()).transpose()
+            is_data_queue_empty = self.data_queue.empty()
+            try:
+                if not is_data_queue_empty:
+                    new_data = self.get_new_data()
 
                     if new_data.empty:
                         logging.warning("Received empty data")
                         continue
 
-                    logging.info("Received new data: %s", new_data.head())
-
-                    self.data = pd.concat([self.data, new_data])
-                    self.data = self.data.drop_duplicates(subset='timestamp').sort_values(by='timestamp').reset_index(
-                        drop=True)
-
-                    logging.info("Data after processing: %s", self.data.head())
+                    self.process_data(new_data)
 
                     processed_data = self.preprocess_data(self.data)
-                    self.train(processed_data, batch_size)  # Pass batch_size
+                    self.train_model(processed_data, batch_size)
                     self.predict(processed_data)
 
+            except ValueError as ve:
+                logging.error("ValueError occurred: %s", traceback.format_exc())
+                # Handle ValueError appropriately
+            except KeyError as ke:
+                logging.error("KeyError occurred: %s", traceback.format_exc())
+                # Handle KeyError appropriately
             except Exception as e:
-                logging.error("Error occurred: %s", str(e))
+                logging.error("Error occurred: %s", traceback.format_exc())
+                # Handle other exceptions appropriately
             time.sleep(1)
         tf.profiler.experimental.stop()
 
+    def get_new_data(self):
+        new_data = pd.DataFrame(self.data_queue.get()).transpose()
 
+        if 'timestamp' not in new_data.columns:
+            if 'time' in new_data.columns:
+                new_data = new_data.rename(columns={"time": "timestamp"})
+            else:
+                logging.warning("Data does not contain 'timestamp' or 'time' column. Skipping...")
+                return pd.DataFrame()
+
+        return new_data
+
+    def process_data(self, new_data):
+        self.data = self.data.append(new_data)
+        self.data = self.data.drop_duplicates(subset='timestamp').sort_values(by='timestamp').reset_index(drop=True)
+        logging.info("Data after processing: %s", self.data.head())
+
+    def check_dataframe_format(self):
+        expected_columns = {
+            'timestamp': 'datetime64[ns]',
+            'symbol': 'category',
+            'side': 'int8',
+            'volume': 'float32',
+            'price': 'float32',
+            'tick_direction': 'int8',
+            'trade_id': 'int64',
+            'is_buy_trade': 'bool',
+            'High': 'float32',
+            'Low': 'float32',
+            'close': 'float32',
+            'RSI': 'float32',
+            'MACD': 'float32',
+            'Bollinger Upper': 'float32',
+            'Bollinger Lower': 'float32',
+            'Stochastic Oscillator': 'float32'
+        }
+
+        for col in expected_columns.keys():
+            if col not in self.data.columns:
+                logging.warning(f"Missing column {col} in data")
+
+        for col in self.data.columns:
+            if col not in expected_columns.keys():
+                logging.warning(f"Unexpected column {col} in data")
+
+        for col in expected_columns.keys():
+            if col not in self.data.columns:
+                continue
+            if str(self.data[col].dtype) != expected_columns[col]:
+                logging.warning(
+                    f"Unexpected data type for column {col}. Expected {expected_columns[col]} but got {self.data[col].dtype}")
+
+        missing_count = self.data.isnull().sum().sum()
+        if missing_count > 0:
+            logging.warning(f"Missing values detected: {missing_count} rows")
+
+        logging.info("Dataframe format checked")
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     data_queue = queue.Queue()
     bot = LandonBot(data_queue)
-    bot.run(batch_size=512)  # Pass desired batch size here
-
-
-
-
-
-
-
+    bot.run(batch_size=512)  # Pass desired batch size her
